a20788a687595150ce21e136efd20d14c154d41e
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-23 16:09:45 -0300

    c3-adding new code

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import {React, useEffect, useState } from "react"
+import useSWR from "swr"
+import Link from "next/link"
 
 
 const Home = () =>{
     
+    const {get} = useSWR('https://jsonplaceholder.typicode.com/users')
+
     const [date, setdata] = useState(0)
+    const [count, setcount] = useState(0)
+
+    useEffect(() => {
+       setInterval(() => {
+           setcount(old => old +1)
+       }, 100);
+}, [])
 
     const Increased = () => {
         setdata(date+1)
@@ -12,12 +23,20 @@ const Home = () =>{
     return <> 
     <h1>Hi there practice little bit about next.js</h1>
     
-     <p> create another thing with react, working with usestate and useEffect</p>
+     <p> create another thing with react, working with usestate and useEffect {count} </p>
 
       
       {date}
      <button onClick={Increased}>+</button>
     
+
+     <pre> {JSON.stringify(get)} </pre>
+ 
+     <Link href='/teste'>
+     <a>NEVER GIVE UP</a>
+     </Link> 
+
+
     </>
 }
 
