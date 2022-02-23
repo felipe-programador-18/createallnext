50cc4827f637e1ac116aae9a9da383c6df2e2b10
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-23 16:09:17 -0300

    c1- adding code and practice about useswr and link

---
+++ pages/teste.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+
+const Here = () =>{
+    return <>
+    
+    <p> YOU HAVE ALWAYS THINK IN FUTURE BETTER FOR YOU, NEVER GIVE UP</p>
+    
+     <Link href='/'>
+     <a> BACK AGAIN</a>
+     </Link>
+
+
+    </>
+}
+
+export default Here
