fa1075f6d9d50125be59a45d7de45144f697e204
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-28 12:40:55 -0300

    c12- create all function and adding getservingprops

--- pages/ssg.js
+++ pages/ssg.js
@@ -1,1 +1,36 @@
 // Practice about site serving generation
+
+const Newspaper = (props) =>{
+
+    return <>
+    
+    <h3>Create test about site serving generation</h3>
+       
+     <pre> {JSON.stringify(props,null, 2)}  </pre>
+    </>
+}
+
+
+export async function getServingSideProps(){
+   return {
+       props: {
+           data: new Date().getDay(),
+           nome:'Programmer fullstack mobile'
+       }
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Newspaper
