056bc1941ad6cf2e92e538b377f3e2b87ccf086d
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-28 11:00:58 -0300

    c5- remember and practice side servering render

---
+++ pages/ssr.js
@@ -0,0 +1,9 @@
+//practice and remeber about side serving rendering
+//is very well because is page lot fast
+// when a page have lot acess is better way to working is side serving rendering
+
+const Ssr = (props) =>{
+ 
+  return ()
+
+}
